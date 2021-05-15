652a8d42cc83eae6bd927f43bcc094bffa84dd4b
_Bare <bare@logixsr.com>
2021-05-15 04:52:43 -0400

    gl render text; needs shader program

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@
 
 // use unicode_width::UnicodeWidthChar;
 
+use std::{
+    collections::HashMap,
+    ffi::c_void,
+    mem::{size_of, size_of_val},
+    ptr::null,
+};
+
+use freetype::{face::LoadFlag, Library};
+
+// use unicode_normalization::UnicodeNormalization;
+
 use glutin::{
     event::{Event as GlutinEvent, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -15,15 +26,122 @@ mod gl {
     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
 }
 
+struct Vec2i {
+    x: i32,
+    y: i32,
+}
+
+struct Character {
+    tex_id: u32,
+    size: Vec2i,
+    bearing: Vec2i,
+    advance: i64,
+}
+
 fn main() {
     let event_loop = EventLoop::new();
-
     let window_builder = WindowBuilder::new().with_title("GL Font Things, Oof!");
-
     let windowed_context =
         make_current_context(ContextBuilder::new().build_windowed(window_builder, &event_loop).unwrap());
 
     gl::load_with(|c_ptr| windowed_context.get_proc_address(c_ptr) as *const _);
+
+    let font_lib = Library::init().unwrap();
+    let font_face = font_lib
+        .new_face("/usr/share/fonts/nerd-fonts-complete/OTF/Fira Code Regular Nerd Font Complete.otf", 0)
+        .unwrap();
+    font_face.set_pixel_sizes(0, 24).unwrap();
+    // font_face.load_char('A' as usize, LoadFlag::RENDER).unwrap();
+    // let glyph = font_face.glyph();
+
+    let mut characters: HashMap<char, Character> = HashMap::new();
+
+    unsafe { gl::PixelStorei(gl::UNPACK_ALIGNMENT, 1) };
+
+    for c in 0..128 {
+        font_face.load_char(c, LoadFlag::RENDER).unwrap();
+
+        let texture = u32::default();
+
+        unsafe {
+            gl::GenTextures(1, texture as *mut _);
+            gl::BindTexture(gl::TEXTURE_2D, texture);
+            gl::TexImage2D(
+                gl::TEXTURE_2D,
+                0,
+                gl::RED as i32,
+                font_face.glyph().bitmap().width(),
+                font_face.glyph().bitmap().rows(),
+                0,
+                gl::RED,
+                gl::UNSIGNED_BYTE,
+                font_face.glyph().bitmap().buffer().as_ptr() as *const std::ffi::c_void,
+            );
+
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, gl::CLAMP_TO_EDGE as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, gl::CLAMP_TO_EDGE as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as i32);
+        };
+
+        let character = Character {
+            tex_id: texture,
+            size: Vec2i { x: font_face.glyph().bitmap().width(), y: font_face.glyph().bitmap().rows() },
+            bearing: Vec2i { x: font_face.glyph().bitmap_left(), y: font_face.glyph().bitmap_top() },
+            advance: font_face.glyph().advance().x,
+        };
+
+        characters.insert(c as u8 as char, character);
+    }
+
+    let (shader_v, shader_f) = (
+        r#"#version 330 core
+layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
+out vec2 TexCoords;
+
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection * vec4(vertex.xy, 0.0, 1.0);
+    TexCoords = vertex.zw;
+}"#,
+        r#"#version 330 core
+in vec2 TexCoords;
+out vec4 color;
+
+uniform sampler2D text;
+uniform vec3 textColor;
+
+void main()
+{    
+    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
+    color = vec4(textColor, 1.0) * sampled;
+}"#,
+    );
+
+    unsafe {
+        gl::Enable(gl::BLEND);
+        gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+    };
+
+    // projection matrix TODO/FIXME
+    // for the shader_program, orthographic that will match our window in this case
+    // let projection = windowed_context.window().inner_size(); // make it a mat4
+    // still need to load shader_program
+
+    let (vao, vbo) = (u32::default(), u32::default());
+    unsafe {
+        gl::GenVertexArrays(1, vao as *mut u32);
+        gl::GenBuffers(1, vbo as *mut u32);
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER, size_of::<f32>() as isize * 6 * 4, null(), gl::DYNAMIC_DRAW);
+        gl::EnableVertexAttribArray(0);
+        gl::VertexAttribPointer(0, 4, gl::FLOAT, gl::FALSE, 4 * size_of::<f32>() as i32, 0 as *const c_void);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    };
 
     event_loop.run(move |event, _window_target, control_flow| {
         *control_flow = ControlFlow::Wait;
@@ -45,11 +163,74 @@ fn main() {
             },
             GlutinEvent::RedrawRequested(_) => {
                 clear_buffer(&Color::eight);
+
+                draw_text(0, "Some text", 50.0, 50.0, 1.0, Color::rgb(128, 0, 0, 255).as_gl(), vao, vbo, &characters);
+
                 windowed_context.swap_buffers().unwrap();
             }
             _ => {}
         }
     });
+}
+
+fn draw_text(
+    shader_program: u32,
+    text: &str,
+    mut x: f32,
+    y: f32,
+    scale: f32,
+    color: glColor,
+    vao: u32,
+    vbo: u32,
+    characters: &HashMap<char, Character>,
+) {
+    // activate shader TODO/FIXME
+    // shader_program.activate();
+
+    unsafe {
+        gl::Uniform3f(
+            gl::GetUniformLocation(shader_program, "textColor".as_ptr() as *const i8),
+            color.r,
+            color.g,
+            color.b,
+        );
+        gl::ActiveTexture(gl::TEXTURE0);
+        gl::BindVertexArray(vao);
+    };
+
+    for c in text.chars() {
+        let character = characters.get(&c).unwrap();
+
+        let xpos = x as f32 + character.bearing.x as f32 * scale;
+        let ypos = y as f32 + (character.size.y - character.bearing.y) as f32 * scale;
+
+        let w = character.size.x as f32 * scale;
+        let h = character.size.y as f32 * scale;
+
+        let verts: [[f32; 4]; 6] = [
+            [xpos, ypos + h, 0.0, 0.0],
+            [xpos, ypos, 0.0, 1.0],
+            [xpos + w, ypos, 1.0, 1.0],
+            [xpos, ypos + h, 0.0, 0.0],
+            [xpos + w, ypos, 1.0, 1.0],
+            [xpos + w, ypos + h, 1.0, 0.0],
+        ];
+
+        unsafe {
+            gl::BindTexture(gl::TEXTURE_2D, character.tex_id);
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::BufferSubData(gl::ARRAY_BUFFER, 0, size_of_val(&verts) as isize, verts.as_ptr() as *const c_void);
+            gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+            gl::DrawArrays(gl::TRIANGLES, 0, 6);
+        };
+
+        x += (character.advance >> 6) as f32 * scale;
+
+        unsafe {
+            gl::BindVertexArray(0);
+            gl::BindTexture(gl::TEXTURE_2D, 0);
+        };
+    }
 }
 
 fn clear_buffer(color: &Color) {
