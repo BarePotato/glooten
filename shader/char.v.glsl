6443097de73eb3014a309aa6aed3682b74cc7592
_Bare <bare@logixsr.com>
2021-09-25 16:04:35 -0400

    toggletest

--- shader/char.v.glsl
+++ shader/char.v.glsl
@@ -3,9 +3,13 @@ layout (location = 0) in vec4 vertex; // <vec2 pos, vec2 tex>
 out vec2 TexCoords;
 
 uniform mat4 projection;
+uniform vec3 kuler;
+
+out vec3 colour;
 
 void main()
 {
+	colour = kuler;
     gl_Position = projection * vec4(vertex.xy, 0.0, 1.0);
     TexCoords = vertex.zw;
 }
